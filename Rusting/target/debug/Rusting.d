7689840a2607e3bea6f272e5eccf6166d9ec5746
Haru Yuumei <luzivotto.erick@gmail.com>
2023-10-23 23:03:24 -0300

    teste

---
+++ Rusting/target/debug/Rusting.d
@@ -0,0 +1,1 @@
+C:\Users\luziv\OneDrive\Área\ de\ Trabalho\Rust\Rusting\target\debug\Rusting.exe: C:\Users\luziv\OneDrive\Área\ de\ Trabalho\Rust\Rusting\src\main.rs
---
+++ Rusting/target/debug/deps/Rusting.d
@@ -0,0 +1,5 @@
+C:\Users\luziv\OneDrive\Área de Trabalho\Rust\Rusting\target\debug\deps\Rusting.exe: src\main.rs
+
+C:\Users\luziv\OneDrive\Área de Trabalho\Rust\Rusting\target\debug\deps\Rusting.d: src\main.rs
+
+src\main.rs:
